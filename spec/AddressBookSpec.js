634dc459c09e7b4897ca11d8aad6fc0167c2af7a
Ruhul Amin <r_amin105@yahoo.com>
2016-11-21 01:09:31 +0100

    refactored spec/AddressBookSpec.js

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,20 +1,18 @@
 describe('Address Book',function () {
     var addressBook,
         thisContact;
+
     beforeEach(function () {
         addressBook = new AddressBook();
         thisContact = new Contact();
-    })
+    });
+
     it('should be able to add address',function () {
-
-
         addressBook.addContact(thisContact);
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
-    it('should be able to delete address',function () {
-        var addressBook = new AddressBook();
-        thisContact = new Contact();
 
+    it('should be able to delete address',function () {
         addressBook.addContact(thisContact);
         addressBook.deleteContact(0);
         expect(addressBook.getContact(0)).not.toBeDefined();
